7eac350c500fac059bd706ff10d54acb1fc3345f
hongtaewon <xodnjs1245@naver.com>
2020-07-09 22:17:06 +0900

    React Component Life Cycle

--- 2.React/movie_app/src/App.js
+++ 2.React/movie_app/src/App.js
@@ -1,51 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({ name, picture, rating}){
-return (<div>
-<h2>I like {name}</h2>
-<h4>{rating}/5.0</h4>
-<img src={picture} alt={name} />
-</div>)
-}
-
-const foodILike = [
-  {
-  id: 1,
-  name: "Kimchi",
-  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
-  },
-  {
-  id: 2,
-  name: "bulgogi",
-  image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM"
-  },
-  {
-  id: 3,
-  name: "kimbap",
-  image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM"
-  },
-  {
-  id: 4,
-  name: "samgyetang",
-  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+//mounting, updating, unmounting <= Component Life Cycle.
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
   }
-];
+  state = {
+    count: -999999999999999
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
+  add = () => {
+    this.setState(current => ({count:current.count+1}));
+  };
+  minus = () => {
+    this.setState(current => ({count:current.count-1}));
+  };
 
-function App() {
-  return (
-  <div>
-    {foodILike.map(dish => (
-    <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/> 
-    ))}
-  </div>
-  );
+
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("component did updated");
+  }
+  componentWillUnmount(){
+    console.log("bye bye");
+  }
+
+  render(){
+    console.log("I`m rendering");
+    return (
+    <div>
+      <h1>The number is {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
+    )
+  }
 }
 
 export default App;

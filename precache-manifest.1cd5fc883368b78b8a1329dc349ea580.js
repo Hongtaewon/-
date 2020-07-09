349fefcc76a112d8fc58da9408d629b5054fd43f
hongtaewon <xodnjs1245@naver.com>
2020-07-09 23:49:21 +0900

    Updates

---
+++ precache-manifest.1cd5fc883368b78b8a1329dc349ea580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56105f07ed64eadbaac0095d3ad985a0",
+    "url": "/-/2.React/movie_app/index.html"
+  },
+  {
+    "revision": "730f1106c33850b50438",
+    "url": "/-/2.React/movie_app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/-/2.React/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/-/2.React/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730f1106c33850b50438",
+    "url": "/-/2.React/movie_app/static/js/main.2da82538.chunk.js"
+  },
+  {
+    "revision": "3598805e3e595def4aa0",
+    "url": "/-/2.React/movie_app/static/js/runtime-main.0104de29.js"
+  }
+]);
